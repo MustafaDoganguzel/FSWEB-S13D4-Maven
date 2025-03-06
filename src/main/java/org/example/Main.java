35ca3ce48c06175a1efdecffbbfad36ed1a6a169
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-03-07 00:02:52 +0300

    Updated Main class to test Point class methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point point1 = new Point(4,5);
+        Point point2 = new Point(3,4);
+        System.out.println("P1 ile P2 arasindaki mesafe" + point1.distance(point2));
+        System.out.println("p1 ile 10,12 arasindaki mesafe " + point2.distance(10,12));
+        System.out.println("p1 in sifir noktasina uzakligi " + point1.distance());
     }
 }
